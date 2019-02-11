6005385319f7e9b639010261ecb27e73c7ccb8f5
sakthivel0531 <sakthivel0531@outlook.com>
2019-02-12 00:32:03 +0530

    Added a loan test project to test loan business logic.  Only project and class files are added.

---
+++ Loan API/Loan.Test/LoanTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class LoanTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetAllLoanDetails_ReturnLoanDetailModel_WhenLoanDetailsExists()
+        {
+            Assert.Pass();
+        }
+    }
+}
